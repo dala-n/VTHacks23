e2e304e89f28cd5a9a8a061f67641748d9d6b8dc
Alex Urbina <alexander.ryan.urbina@gmail.com>
2023-09-16 12:24:46 -0400

    changed html and added search function

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from search import search_demographics  # Import the search function
+import csv
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        zipcode = request.form["zipcode"]
+        # Call the search_demographics function to retrieve data based on the ZIP code.
+        # Store the data in a variable.
+        data = search_demographics(zipcode)
+        return render_template("index.html", data=data)
+    return render_template("index.html", data=None)
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
