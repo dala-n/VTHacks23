231d4b31fae77fd31e7e3b3e72a91cce88ff636d
Alex Urbina <alexander.ryan.urbina@gmail.com>
2023-09-16 16:03:07 -0400

    pie chart added

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 from search import search_demographics  # Import the search function
 import csv
+import matplotlib.pyplot as plt
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -8,11 +11,27 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         zipcode = request.form["zipcode"]
-        # Call the search_demographics function to retrieve data based on the ZIP code.
+        # Call the search_demographics function to retrieve percentage data based on the ZIP code.
         # Store the data in a variable.
         data = search_demographics(zipcode)
-        return render_template("index.html", data=data)
+
+        # Create a pie chart using Matplotlib
+        labels = ['White', 'Black', 'Hispanic', 'Foreign']
+        sizes = [data["White"], data["Black"], data["Hispanic"], data["Foreign"]]
+        colors = ['red', 'blue', 'green', 'yellow']
+        plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
+        plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
+        
+        # Save the pie chart as an image file
+        img_buf = io.BytesIO()
+        plt.savefig(img_buf, format='png')
+        img_buf.seek(0)
+        img_base64 = base64.b64encode(img_buf.read()).decode('utf-8')
+
+        return render_template("index.html", data=data, img_base64=img_base64)
+    
     return render_template("index.html", data=None)
+
 
 @app.route("/map", methods=["GET", "POST"])
 def map():
