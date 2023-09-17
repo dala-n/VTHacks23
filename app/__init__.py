a2b934c312c12a2da7706bb474a71b1dd1148752
BuildTools <unconfigured@null.spigotmc.org>
2023-09-16 20:19:31 -0400

    added filter for pie chart

--- app/__init__.py
+++ app/__init__.py
@@ -14,33 +14,34 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         zipcode = request.form["zipcode"]
-        # Call the search_demographics function to retrieve percentage data based on the ZIP code.
-        # Store the data in a variable.
-        data = search_demographics(zipcode)
+        data_type = request.form["data-type"]  # Get the selected data type
+        data = search_demographics(zipcode, data_type)  # Filter data based on data type
 
-        # Create a new figure and axis for the pie chart using Matplotlib
-        fig, ax = plt.subplots()
-        labels = ['White', 'Black', 'Hispanic', 'Foreign']
-        sizes = [data["White"], data["Black"], data["Hispanic"], data["Foreign"]]
-        colors = ['red', 'blue', 'green', 'yellow']
-        ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
-        ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
+        if data:
+            # Create a new figure and axis for the pie chart using Matplotlib
+            fig, ax = plt.subplots()
+            labels = list(data.keys())[1:]  # Exclude "ZIP Code" from labels
+            sizes = list(data.values())[1:]
+            colors = ['red', 'blue', 'green', 'yellow']
+            ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
+            ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
 
-        # Save the pie chart as an image file
-        img_buf = io.BytesIO()
-        plt.savefig(img_buf, format='png')
-        img_buf.seek(0)
-        img_base64 = base64.b64encode(img_buf.read()).decode('utf-8')
+            # Save the pie chart as an image file
+            img_buf = io.BytesIO()
+            plt.savefig(img_buf, format='png')
+            img_buf.seek(0)
+            img_base64 = base64.b64encode(img_buf.read()).decode('utf-8')
 
-        # Close the Matplotlib figure to free up resources
-        plt.close(fig)
+            # Close the Matplotlib figure to free up resources
+            plt.close(fig)
 
-        # Close the image buffer
-        img_buf.close()
+            # Close the image buffer
+            img_buf.close()
 
-        return render_template("index.html", data=data, img_base64=img_base64)
+            return render_template("index.html", data=data, img_base64=img_base64)
     
     return render_template("index.html", data=None)
+
 
 
 
--- app/search.py
+++ app/search.py
@@ -1,32 +1,33 @@
 import csv
 
-def search_demographics(zipcode):
+def search_demographics(zipcode, data_type):
     data = None
     with open("app/data/demographic_data.csv", mode="r") as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
             if row["zcta10"] == zipcode:
-                data = {
-                    # "ZIP Code": row["zcta10"],
-                    # "Total Population (13-17)": row["totpop13_17"],
+                data = {}
 
-                    # ethnicity
-                    "White": float(row["pnhwhite13_17"]),
-                    "Black": float(row["pnhblack13_17"]),
-                    "Hispanic": float(row["phispanic13_17"]),
-                    "Foreign": float(row["pfborn13_17"]),
-                    
-                    # income
-                    "15-30K": float(row["pin2b_13_17"]),
-                    "30-50K": float(row["pin3b_13_17"]),
-                    "50-100K": float(row["pin4b_13_17"]),
-                    "100K+": float(row["pin5b_13_17"]),
+                if data_type == "ethnicity":
+                    data.update({
+                        "White": float(row["pnhwhite13_17"]),
+                        "Black": float(row["pnhblack13_17"]),
+                        "Hispanic": float(row["phispanic13_17"]),
+                        "Foreign": float(row["pfborn13_17"])
+                    })
+                elif data_type == "income":
+                    data.update({
+                        "15-30K": float(row["pin2b_13_17"]),
+                        "30-50K": float(row["pin3b_13_17"]),
+                        "50-100K": float(row["pin4b_13_17"]),
+                        "100K+": float(row["pin5b_13_17"])
+                    })
+                elif data_type == "education":
+                    data.update({
+                        "Less than High School Diploma": float(row["ped1_13_17"]),
+                        "High School Diploma and/or Some College": float(row["ped2_13_17"]),
+                        "Bachelor's Degree or Higher": float(row["ped3_13_17"])
+                    })
 
-                    # education
-                    "Less than High School Diploma": float(row["ped1_13_17"]),
-                    "High School Diploma and/or Some College": float(row["ped2_13_17"]),
-                    "Bachelor's Degree or Higher": float(row["ped3_13_17"]),
-
-                }
                 break  # Stop searching after finding a matching ZIP code
     return data
