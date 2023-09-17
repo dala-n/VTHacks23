d33fb6e7e7e62848bf155b1077e8966dafd25306
BuildTools <unconfigured@null.spigotmc.org>
2023-09-16 20:05:35 -0400

    added education and income

--- app/search.py
+++ app/search.py
@@ -7,14 +7,26 @@ def search_demographics(zipcode):
         for row in csv_reader:
             if row["zcta10"] == zipcode:
                 data = {
-                    "ZIP Code": row["zcta10"],
-                    "Total Population (13-17)": row["totpop13_17"],
+                    # "ZIP Code": row["zcta10"],
+                    # "Total Population (13-17)": row["totpop13_17"],
+
+                    # ethnicity
                     "White": float(row["pnhwhite13_17"]),
                     "Black": float(row["pnhblack13_17"]),
                     "Hispanic": float(row["phispanic13_17"]),
                     "Foreign": float(row["pfborn13_17"]),
                     
-                    # Add other demographic data here
+                    # income
+                    "15-30K": float(row["pin2b_13_17"]),
+                    "30-50K": float(row["pin3b_13_17"]),
+                    "50-100K": float(row["pin4b_13_17"]),
+                    "100K+": float(row["pin5b_13_17"]),
+
+                    # education
+                    "Less than High School Diploma": float(row["ped1_13_17"]),
+                    "High School Diploma and/or Some College": float(row["ped2_13_17"]),
+                    "Bachelor's Degree or Higher": float(row["ped3_13_17"]),
+
                 }
                 break  # Stop searching after finding a matching ZIP code
     return data
